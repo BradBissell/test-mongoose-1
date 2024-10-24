45e07978be8ff794a7268caeb991bcd3f49bbf57
bissbr01 <bissell.brad@gmail.com>
2024-10-23 21:00:34 -0400

    working locally

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,19 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  if (process.env.APP_ENV === 'prod') {
+    app.enableCors({
+      origin: process.env.CORS_ORIGIN,
+      methods: '*',
+    });
+  } else {
+    app.enableCors({
+      origin: '*',
+      methods: '*',
+    });
+  }
+
   await app.listen(3000);
 }
 bootstrap();
